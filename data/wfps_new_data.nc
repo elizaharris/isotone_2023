05893c07ed8e45256b4189bf286580b3f0a97f59
Eliza Harris <harris@kspc14.unibe.ch>
2025-02-07 11:08:08 +0100

    trying to fix commit issue on new wfps regridding

---
+++ data/wfps_new_data.nc
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:bc811b9600e632e1d792f365415895d7531ae9c5ab9765a592ba75a999ef3192
+size 1520683368
---
+++ src/regrid_WFPS_fromDan.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 24 09:50:35 2020
+
+This file imports and regrids WFPS from Dan (not with other inputs; added later and annual)
+
+@author: elizaharris
+
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import math as math
+import xarray as xr
+import cartopy.crs as ccrs
+from datetime import datetime as dt
+import rasterio as rio
+import os
+from scipy.interpolate import griddata
+
+parentdir = os.getcwd()
+os.chdir(parentdir+"/isotone_arcticBranch") # Change to the isotone repo as working directory
+from src.utils import plot_map
+from src.utils import rast_to_list
+import configs as configs
+
+# set resolution and grid for model
+resolution = configs.res["ystart"]
+lat_out = np.arange(configs.res["latstart"], configs.res["latend"], configs.res["latres"]) # min max spacing
+lon_out = np.arange(configs.res["lonstart"], configs.res["lonend"], configs.res["lonres"])
+(LON,LAT) = np.meshgrid(lon_out,lat_out)
+
+#%% Global AI for ocean mask... (not needed, no ocean mask required, copy again from NH3 regrid if needed)
+
+#%% Get the WFPS and check
+    
+wfps_rast = rio.open("../isotone-rawdata/wfps_0-7cm/wfps0007_globalmap_2020.tif")
+wfps_list = rast_to_list(wfps_rast,5,dataclip=(0,np.inf),takemean="N") # data as list
+# regrid the data from 1D back to 2D and to a rougher grid (too many values for now otherwise)
+wfps_grid = griddata((wfps_list[:,0],wfps_list[:,1]), wfps_list[:,2], (LON,LAT), method='linear')
+# wfps_grid = (wfps_grid).clip(0,100)*ocean_mask # not needed
+plot_map(LON,LAT,wfps_grid,title="wfps",filename="figs/input_data/wfps_0-7cm_2020")
+
+#%% Get file names
+
+from os import listdir
+from os.path import isfile, join
+wfps_filenames_007 = [f for f in listdir("../isotone-rawdata/wfps_0-7cm/") if isfile(join("../isotone-rawdata/wfps_0-7cm/", f))]
+wfps_filenames_728 = [f for f in listdir("../isotone-rawdata/wfps_7-28cm/") if isfile(join("../isotone-rawdata/wfps_7-28cm/", f))]
+nyears = len(wfps_filenames_007)
+
+# Make sure ordering correct
+years_007 = np.array([ int(s.split("_")[2].split(".")[0]) for s in wfps_filenames_007 ])
+order = np.argsort(years_007)
+years_007 = years_007[order]
+wfps_filenames_007 = np.array(wfps_filenames_007)[order]
+years_728 = np.array([ int(s.split("_")[2].split(".")[0]) for s in wfps_filenames_728 ])
+order = np.argsort(years_728)
+years_728 = years_728[order]
+wfps_filenames_728 = np.array(wfps_filenames_728)[order]
+
+# Check years
+if len(wfps_filenames_728)!=len(wfps_filenames_007):
+    print("Different numbers of files for soil depths!")
+if np.nanmin(years_007)!=np.nanmin(years_728):
+    print("Different start years for different depths")
+years = years_007.copy()
+
+#%% Import and regrid each file
+wfps_007 = np.zeros((wfps_grid.shape[0],wfps_grid.shape[1],nyears))*np.nan
+for n,s in enumerate(wfps_filenames_007):
+    print(s)
+    wfps_rast = rio.open("../isotone-rawdata/wfps_0-7cm/"+s)
+    wfps_list = rast_to_list(wfps_rast,5,dataclip=(0,np.inf),takemean="N") # data as list
+    wfps_grid = griddata((wfps_list[:,0],wfps_list[:,1]), wfps_list[:,2], (LON,LAT), method='linear')
+    wfps_007[:,:,n] = wfps_grid
+    if years[n]==2020:
+        plot_map(LON,LAT,wfps_007[:,:,n],title="wfps",filename="figs/input_data/wfps_0-7cm_2020")
+wfps_007_mean = np.nanmean(wfps_007,axis=1)
+
+#%% Save each year of the data as a netcdf file
+
+import netCDF4 as nc4
+
+ncout = nc4.Dataset('data/wfps_new_data.nc','w','NETCDF4'); # using netCDF3 for output format 
+ncout.createDimension('lon',LON.shape[1])
+ncout.createDimension('lat',LAT.shape[0])
+ncout.createDimension('year',nyears)
+lonvar = ncout.createVariable('lon','f4',('lon'))
+lonvar[:] = LON[0,:]
+lonvar.setncattr('units','degrees east')
+latvar = ncout.createVariable('lat','f4',('lat'))
+latvar[:] = LAT[:,0]
+latvar.setncattr('units','degrees north')
+yearvar = ncout.createVariable('year','f4',('year'))
+yearvar[:] = years
+yearvar.setncattr('units','year')
+wfps = ncout.createVariable('wfps','f4',('lat','lon','year'))
+wfps.setncattr('units','percentage')
+wfps[:,:,:] = wfps_007[:,:]
+ncout.close()
+
+# test the file!
+f = nc4.Dataset('data/wfps_new_data.nc','r')
+plot_map(LON,LAT,f.variables["wfps"][:,:,0],"wfps")
+plot_map(LON,LAT,f.variables["wfps"][:,:,20],"wfps")
